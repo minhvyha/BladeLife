e9495d964ba1db992d60b9b17767e1d55696e90d
Minh Vy ha <minhvy.ha@outlook.com>
2026-02-14 13:28:50 +1100

    feat: replace Snowflake component with SVG images in PageHeader

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -3,8 +3,10 @@ import { Snowflake } from './snowflake'
 export function PageHeader({ title }: { title: string }) {
   return (
     <div className="relative mb-6">
-      <Snowflake className="absolute -top-2 right-8 w-10 h-10" />
-      <Snowflake className="absolute top-8 left-4 w-8 h-8" />
+      <img src="/snowflake.svg" alt="" className="absolute -top-2 left-8 w-10 h-10" />
+      <img src="/snowflake.svg" alt="" className="absolute bottom-2 right-8 w-10 h-10" />
+      <img src="/snowflake.svg" alt="" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10" />
+
       <h1 className="text-3xl font-bold text-center text-balance">{title}</h1>
     </div>
   )
